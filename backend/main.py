b9e124341902927b79bf9ca4901d96692cbc7109
sayak53 <sayakchakraborty928@gmail.com>
2026-05-07 10:52:10 +0530

    installed sckit-learn to analyze the score

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
 from PyPDF2 import PdfReader
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
 app = FastAPI()
 
 app.add_middleware(
@@ -14,14 +17,31 @@ app.add_middleware(
 
 def extract_text(file):
     reader = PdfReader(file)
+
     text = ""
+
     for page in reader.pages:
         text += page.extract_text()
+
     return text
+
+
+def get_similarity(resume, job_description):
+    documents = [resume, job_description]
+
+    cv = CountVectorizer()
+
+    matrix = cv.fit_transform(documents)
+
+    similarity = cosine_similarity(matrix)[0][1]
+
+    return round(similarity * 100, 2)
+
 
 @app.get("/")
 def home():
     return {"message": "Backend is running 🚀"}
+
 
 @app.post("/analyze/")
 async def analyze_resume(
@@ -30,8 +50,10 @@ async def analyze_resume(
 ):
     text = extract_text(resume.file)
 
+    score = get_similarity(text, job_description)
+
     return {
-        "message": "File received",
+        "score": score,
         "resume_preview": text[:300],
-        "job_description": job_description[:100]
+        "job_description": job_description
     }
