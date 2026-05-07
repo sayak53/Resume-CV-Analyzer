52c5333cc1a0c2bf3e00adaac50d299151cff8eb
sayak53 <sayakchakraborty928@gmail.com>
2026-05-07 10:36:24 +0530

    fixed cors

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.middleware.cors import CORSMiddleware
 from PyPDF2 import PdfReader
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def extract_text(file):
     reader = PdfReader(file)
