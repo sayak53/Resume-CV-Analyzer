7b06327db822296405ed8679a00fad05e5e01c5b
sayak53 <sayakchakraborty928@gmail.com>
2026-05-19 14:09:16 +0530

    connected frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,10 +22,13 @@ function App() {
     formData.append("job_description", jobDescription);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/analyze/", {
-        method: "POST",
-        body: formData,
-      });
+      const response = await fetch(
+        "https://resume-analyzer-backend-osg2.onrender.com/analyze/",
+        {
+          method: "POST",
+          body: formData,
+        },
+      );
 
       const data = await response.json();
 
